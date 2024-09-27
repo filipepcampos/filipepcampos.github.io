da9a6da23af31793bd0eede767c547a9cb4fd3cd
Filipe Campos <filipepcampos24@gmail.com>
2024-09-28 00:59:04 +0200

    add accordion sections for uni tables

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -1,4 +1,4 @@
-import GradeTable from "@/components/GradeTable";
+import UniversitySemesterSection from "@/components/UniversitySemesterSection";
 
 export default function Education() {
   const firstYear = [
@@ -37,7 +37,7 @@ export default function Education() {
         {
           name: "Checkers",
           url: "https://github.com/filipepcampos/feup-sgi-proj",
-          weburl: "https://sgi.projects.filipepcampos.com/tp3/"
+          weburl: "https://sgi.projects.filipepcampos.com/tp3/",
         },
       ],
     },
@@ -111,12 +111,12 @@ export default function Education() {
         },
         {
           name: "DuckieBot Follower Environment",
-          url: "https://github.com/filipepcampos/feup-ri-assign4"
+          url: "https://github.com/filipepcampos/feup-ri-assign4",
         },
         {
           name: "DuckieBot Follower Model",
-          url: "https://github.com/filipepcampos/feup-ri-assign4-model"
-        }
+          url: "https://github.com/filipepcampos/feup-ri-assign4-model",
+        },
       ],
     },
     {
@@ -156,22 +156,22 @@ export default function Education() {
       ects: "B",
       projects: [
         {
-          "name": "Dissertation Code",
-          "url": "https://github.com/filipepcampos/anonymous-case-based-explainability"
-        }
+          name: "Dissertation Code",
+          url: "https://github.com/filipepcampos/anonymous-case-based-explainability",
+        },
       ],
     },
   ];
 
   const bscFirstYear = [
-    { uc: "Algebra", grade: 18, ects: "B"},
+    { uc: "Algebra", grade: 18, ects: "B" },
     { uc: "Mathematical Analysis", grade: 19, ects: "A" },
     {
       uc: "Computer Architecture and Organization",
       grade: 17,
       ects: "A",
     },
-    { uc: "Complements of Mathematics", grade: 18, ects: "A"},
+    { uc: "Complements of Mathematics", grade: 18, ects: "A" },
     { uc: "Physics I", grade: 17, ects: "B" },
     {
       uc: "Programming Fundamentals",
@@ -184,7 +184,7 @@ export default function Education() {
         },
       ],
     },
-    { uc: "Discrete Mathematics", grade: 19, ects: "A"},
+    { uc: "Discrete Mathematics", grade: 19, ects: "A" },
     {
       uc: "Statistical Methods",
       grade: 18,
@@ -396,7 +396,12 @@ export default function Education() {
       uc: "Capstone Project",
       grade: 18,
       ects: "A",
-      projects: [{ name: "Mobile Car Driving", url: "https://github.com/filipepcampos/mobile-car-driving-model" }],
+      projects: [
+        {
+          name: "Mobile Car Driving",
+          url: "https://github.com/filipepcampos/mobile-car-driving-model",
+        },
+      ],
     },
     {
       uc: "Artificial Intelligence",
@@ -419,52 +424,34 @@ export default function Education() {
     <section className="bg-white dark:bg-gray-900">
       <div className="grid grid-cols-1 gap-16 max-w-screen-xl min-y-screen-xl px-8 py-8 mx-auto">
         <section id="msc">
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <div className="relative overflow-x-auto sm:rounded-lg">
             <div className="p-5 text-2xl font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-              MSc in Informatics and Computing Engineering
+              <h2>MSc in Informatics and Computing Engineering</h2>
               <p className="mt-1 text-lg font-normal text-gray-500 dark:text-gray-400">
                 Below is an overview of the work done during my studies.
               </p>
             </div>
-            <div className="mb-8">
-              <h3 className="p-5 text-lg font-semibold text-gray-900 dark:text-white">
-                1st Year
-              </h3>
-              <GradeTable data={firstYear} />
-            </div>
-            <div className="mb-8">
-              <h3 className="p-5 text-lg font-semibold text-gray-900 dark:text-white">
-                2nd Year
-              </h3>
-              <GradeTable data={secondYear} />
+            <div className="flex flex-col gap-4">
+              <UniversitySemesterSection title="1st Year" data={firstYear} />
+              <UniversitySemesterSection title="2nd Year" data={secondYear} />
             </div>
           </div>
         </section>
         <section id="bsc">
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <div className="relative overflow-x-auto sm:rounded-lg">
             <div className="p-5 text-2xl font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
               <h2>BSc in Informatics and Computing Engineering</h2>
               <p className="mt-1 text-lg font-normal text-gray-500 dark:text-gray-400">
                 Below is an overview of the work done during my studies.
               </p>
             </div>
-            <div className="mb-8">
-              <h3 className="p-5 text-lg font-semibold text-gray-900 dark:text-white">
-                1st Year
-              </h3>
-              <GradeTable data={bscFirstYear} />
-            </div>
-            <div className="mb-8">
-              <h3 className="p-5 text-lg font-semibold text-gray-900 dark:text-white">
-                2nd Year
-              </h3>
-              <GradeTable data={bscSecondYear} />
-            </div>
-            <div>
-              <h3 className="p-5 text-lg font-semibold text-gray-900 dark:text-white">
-                3rd Year
-              </h3>
-              <GradeTable data={bscThirdYear} />
+            <div className="flex flex-col gap-4">
+              <UniversitySemesterSection title="1st Year" data={bscFirstYear} />
+              <UniversitySemesterSection
+                title="2nd Year"
+                data={bscSecondYear}
+              />
+              <UniversitySemesterSection title="3rd Year" data={bscThirdYear} />
             </div>
           </div>
         </section>
